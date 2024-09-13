f4ed3d98f3928ce4e7ba453c753392f915300e62
AshiqTasdid <ashiqtasdid5@gmail.com>
2024-09-13 22:33:43 +0600

    Added, Solar

--- app/neo/page.tsx
+++ app/neo/page.tsx
@@ -26,6 +26,7 @@ const generateSatellites = (count: number) => {
   });
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 const SatelliteParticles = ({ satelliteData }: { satelliteData: any[] }) => {
   const ref = useRef<THREE.Points>(null!);
 
---
+++ app/solar/page.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import SolarSystem from "@/components/SolarSystem";
+
+
+const Home: React.FC = () => {
+
+
+  return (
+    <div className="m-0 p-0 overflow-hidden w-[100%] h-[100%]" id="__next">
+      <main className="">
+        <div className="">
+          <SolarSystem />
+
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
